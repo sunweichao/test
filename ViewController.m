9fa13258f754b0c92f977eaf3b302ab8ef9e1d78
Tree8 <747482481@qq.com>
2016-10-31 14:31:14 +0800

    增加输出
    22222

--- ViewController.m
+++ ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"孙玮超");
+    NSLog(@"xixi");
 }
 
 - (void)didReceiveMemoryWarning {
