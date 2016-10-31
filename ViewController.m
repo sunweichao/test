b8928240ce60925cd1adfbd934bbcd989ff18ce4
linhaoran <546344877@qq.com>
2016-10-31 14:30:21 +0800

    加名字

--- ViewController.m
+++ ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"孙玮超");
+    NSLog(@"林昊然");
 }
 
 - (void)didReceiveMemoryWarning {
